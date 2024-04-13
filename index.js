3c82e7cd139f100ee6426717ef983e18b68c4bf3
gayatriwagh2003 <139702601+gayatriwagh2003@users.noreply.github.com>
2024-04-13 08:48:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ let mul=(x,y)=>
 mul(3,8);
 console.log("Hii mayuri");
 console.log("Hii gayatri");
+function display()
+{
+    let a=8;
+    let b=10;
+    let div=b/a;
+    console.log(div);
+}
+display(div);
